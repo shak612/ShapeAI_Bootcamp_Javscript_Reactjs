3bb210f0f119e1cfe95952c9d8d90f5903f6edb1
shak612 <82748745+shak612@users.noreply.github.com>
2021-06-26 16:56:30 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div>
+      <div className="note">
+        <h1>Javascript and React.js - Free 7 Days Long Bootcamp</h1>
+        <p>
+          Here we bring our another free live BOOTCAMP with a hands-on project
+          at the end; Javascript and React.js.
+        </p>
+      </div>
+
+      <div className="note">
+        <h1>Tableau Fundamentals - Free 7 Days Long Bootcamp</h1>
+        <p>
+          Here we bring our another free live BOOTCAMP with a hands-on project
+          at the end; Tableau Fundamentals.
+        </p>
+      </div>
+
+      <div className="note">
+        <h1>Javascript and Backend with NodeJS - 7 Day Bootcamp</h1>
+        <p>
+          Here we bring our another free live BOOTCAMP with a hands-on project
+          at the end; Javascript & NodeJS.
+        </p>
+      </div>
+
+      <div className="note">
+        <h1>Python and Natural Language Processing - 7 Day Bootcamp</h1>
+        <p>
+          Here we bring our another free live BOOTCAMP with a hands-on project
+          at the end; Python & Natural Language
+        </p>
+      </div>
+
+      <div className="note">
+        <h1>Python & Network Security - Free 7 Days Long Bootcamp</h1>
+        <p>
+          Here we bring our another free live BOOTCAMP with a hands-on project
+          at the end; Python and Networking Security
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Info;
